db302ac461e0142c40a3eb85b208c8e6db877cf9
Jamie Gaehring <gaehj457@gmail.com>
2019-06-17 14:28:06 -0400

    add service worker

---
+++ precache-manifest.6013689b1e95627ec6e294a75dbb9ab6.js
@@ -0,0 +1,16 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cfef56d797afe8e8a81c9768d56768d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38dfe180ed0674a9cd8",
+    "url": "/static/css/app.802fd10775cfae6823b7.css"
+  },
+  {
+    "url": "/static/js/1.6a0a584ffd22a718db0d.js"
+  },
+  {
+    "url": "/static/js/app.f38dfe180ed0674a9cd8.js"
+  }
+]);
