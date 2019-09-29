b11eea64cd2a7b050da5482f730112f0a7fdbb2a
Jamie Gaehring <gaehj457@gmail.com>
2019-09-29 15:51:05 -0400

    Build v0.4.8

---
+++ precache-manifest.57d442060df3761f5ca1ca6318bc8727.js
@@ -0,0 +1,20 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e9af64c5069e0082c052374410a915f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88fddf8e3dbe570a0351",
+    "url": "/static/css/app.c677fea0ee0c795e0950.css"
+  },
+  {
+    "revision": "eeda3d837d12b900ee05",
+    "url": "/static/css/vendor.ff1bd8880ab64c30fc24.css"
+  },
+  {
+    "url": "/static/js/1.eeda3d837d12b900ee05.js"
+  },
+  {
+    "url": "/static/js/app.88fddf8e3dbe570a0351.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b437879900aaad8db03d1a3a05caea9.js"
+  "/precache-manifest.57d442060df3761f5ca1ca6318bc8727.js"
 );
 
 self.addEventListener('message', (event) => {
